c54e9f2cabdea032b557c3c611166ae70d122ad4
handezVN <handez1008@gmail.com>
2020-12-08 10:35:15 +0700

    Login

---
+++ client/src/components/Login.js
@@ -0,0 +1,103 @@
+import axios from 'axios';
+import React,{useState} from 'react'
+
+export default function Login({setIsLogin}) {
+
+    const [user , setUser] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const [err,setErr] = useState('')
+    const onChangeInput = e =>{
+        const {name , value } = e.target;
+        setUser ({...user,[name]:value })
+        setErr('')
+    }
+    const registerSubmit = async e =>{
+        e.preventDefault()
+        try {
+            const res = await axios.post('/users/register',{
+                username: user.name , 
+                email: user.email,
+                password: user.password
+            }) 
+            setUser({name: '' , email: '' , password: ''})
+            setErr(res.data.msg)
+        } catch (error) {
+            error.response.data.msg && setErr(error.response.data.msg)
+        }
+    } 
+
+    const loginSubmit = async e => {
+        e.preventDefault()
+        try {
+            const res = await axios.post('/users/login',{
+               
+                email: user.email,
+                password: user.password
+            }) 
+            setUser({name: '' , email: '' , password: ''})
+            localStorage.setItem('tokenStore',res.data.token)
+            setIsLogin(true)
+        } catch (error) {
+             error.response.data.msg && setErr(error.response.data.msg)
+        }
+    }
+    return (
+        <section>
+            <div className="login">
+                <h2>Login</h2>
+                <form onSubmit={loginSubmit}> 
+                    <input type="email" 
+                    name="email" 
+                    id="login-email" 
+                    placeholder="Email" 
+                    required value ={user.email} onChange={onChangeInput}/>
+
+                    <input type="password" 
+                    name="password" 
+                    id="login-password" 
+                    placeholder="Password" 
+                    required value ={user.password} autoComplete="true" onChange={onChangeInput}/>
+
+                    <button type="submit">Login</button>
+                    <p>You dont have an account?
+                        <span>Register Now</span>
+                    </p>
+                     <h3>{err}</h3>
+                </form>
+            </div>
+            <div className="register">
+                <h2>Register</h2>
+                <form onSubmit={registerSubmit}>
+                    <input type="text" 
+                    name="name" 
+                    id="register-name" 
+                    placeholder="User Name" 
+                    required value ={user.name} onChange={onChangeInput}/>
+
+                    <input type="email" 
+                    name="email" 
+                    id="register-email" 
+                    placeholder="Email" 
+                    required value ={user.email} onChange={onChangeInput}/>
+
+                    <input type="password" 
+                    name="password" 
+                    id="register-password" 
+                    placeholder="Password" 
+                    required value ={user.password} autoComplete="true" onChange={onChangeInput}/>
+
+                    <button type="submit">Register</button>
+                    <p>You have an account?
+                        <span>Login Now</span>
+                    </p> 
+                    <h3>{err}</h3>
+                </form>
+            </div>
+        </section>
+    )
+}
+
